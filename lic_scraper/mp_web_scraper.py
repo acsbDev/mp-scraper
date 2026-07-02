66fbe1e297dac9b21d90219a5c259a11ff65be81
Jose M <jmmejias11@gmail.com>
2026-07-02 08:34:00 -0400

    upsi

--- lic_scraper/mp_web_scraper.py
+++ lic_scraper/mp_web_scraper.py
@@ -50,8 +50,8 @@ class MPWebScraper:
         options = Options()
 
         if headless:
-            # options.add_argument("--headless")
-            # options.add_argument("--window-size=1920,1080")
+            options.add_argument("--headless")
+            options.add_argument("--window-size=1920,1080")
             options.add_argument("--disable-gpu")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
